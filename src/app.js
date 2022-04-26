00a254fe35b130c3d0ac522259ec4742336d2f66
i-am-sam-codes <sammmceja@gmail.com>
2022-04-26 15:56:54 -0700

    add check-mark

--- src/app.js
+++ src/app.js
@@ -1,28 +1,26 @@
-let library = []
+let library = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-    library.push(book);
+  const book = new Book(title, author, pages, read);
+  library.push(book);
 }
 
-addBookToLibrary("Book1", "by me", 20, true)
-addBookToLibrary("Book2", "by you", 200, false)
-
+addBookToLibrary("Book1", "by me", 20, true);
+addBookToLibrary("Book2", "by you", 200, false);
 
 function displayBooks() {
-    for(const book of library) {
-        if(book.length === 0)break
-        console.log(book)
-    }
+  for (const book of library) {
+    if (book.length === 0) break;
+    console.log(book);
+  }
 }
-displayBooks()
-
+displayBooks();
 
 // Get the modal
 const modal = document.getElementById("myModal");
@@ -34,18 +32,18 @@ const btn = document.getElementById("myBtn");
 const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
