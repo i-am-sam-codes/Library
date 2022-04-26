83203f194716cf997444dd499eeb4553ff303bbb
i-am-sam-codes <sammmceja@gmail.com>
2022-04-25 20:10:16 -0700

    Add modal/input list pop up

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ function addBookToLibrary(title, author, pages, read) {
     library.push(book);
 }
 
-addBookToLibrary("b", "b", 20, true)
-addBookToLibrary("c", "c", 200, false)
+addBookToLibrary("Book1", "by me", 20, true)
+addBookToLibrary("Book2", "by you", 200, false)
 
 
 function displayBooks() {
@@ -22,3 +22,30 @@ function displayBooks() {
     }
 }
 displayBooks()
+
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
